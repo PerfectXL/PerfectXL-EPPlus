ca38b5cfec37ff90c1a3d36855db6083ad57ae53
Willem-Jan Meerkerk <willemjanmeerkerk@hotmail.com>
2020-02-06 16:11:37 +0100

    Fix: raw formula of defined names was not copied

--- PerfectXL.EPPlus/ExcelWorksheets.cs
+++ PerfectXL.EPPlus/ExcelWorksheets.cs
@@ -345,7 +345,8 @@ namespace OfficeOpenXml
                 {
                     newName=added.Names.AddValue(name.Name, name.Value);
                 }
-               newName.NameComment = name.NameComment;
+                newName.RawFormula = name.RawFormula;
+                newName.NameComment = name.NameComment;
             }
         }
 
